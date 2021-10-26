d53d8b394f1d870e3610b5d6359a0ac1b03421b1
jqntn <jqntn88@gmail.com>
2021-10-26 12:34:53 +0200

    fix: Unique.cs

--- Assets/StickIt/Scripts/Unique.cs
+++ Assets/StickIt/Scripts/Unique.cs
@@ -1,6 +1,14 @@
 using UnityEngine;
 abstract class Unique<T> : MonoBehaviour where T : class
 {
-    public static T _instance;
-    void Awake() { if (_instance == null) { _instance = GameObject.FindObjectOfType(typeof(T)) as T; DontDestroyOnLoad(gameObject); } else Destroy(gameObject); }
+    public static T instance;
+    protected virtual void Awake()
+    {
+        if (instance == null)
+        {
+            instance = GameObject.FindObjectOfType(typeof(T)) as T;
+            DontDestroyOnLoad(gameObject);
+        }
+        else Destroy(gameObject);
+    }
 }
