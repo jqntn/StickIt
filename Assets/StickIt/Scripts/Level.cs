848b6f5e77b808e54578195844755d4a1b600e8b
jqntn <jqntn88@gmail.com>
2021-11-08 18:56:32 +0100

    fixes

--- Assets/StickIt/Scripts/Level.cs
+++ Assets/StickIt/Scripts/Level.cs
@@ -1,25 +1,8 @@
-using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.SceneManagement;
 public class Level : MonoBehaviour
 {
-    // Start is called before the first frame update
-
     public List<Player> winners;
-    public Transform startingPos;
-
-    void Start()
-    {
-        Init();
-    }
-
-    public virtual void Init()
-    {
-    }
-
-    public virtual void EndMap()
-    {
-        Debug.Log("End Map");
-    }
+    public virtual void Init() { }
+    public virtual void EndMap() { }
 }
