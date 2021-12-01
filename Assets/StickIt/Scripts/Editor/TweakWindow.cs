b2a0ee1c463bcdc9ece1d5d7e513505c97ddbf17
AlexCopin <alexcopin@live.fr>
2021-12-01 17:54:17 +0100

    TweakWindowContinuing

--- Assets/StickIt/Scripts/Editor/TweakWindow.cs
+++ Assets/StickIt/Scripts/Editor/TweakWindow.cs
@@ -3,11 +3,25 @@ using UnityEditor;
 
 public class TweakWindow : EditorWindow
 {
+    public bool showPlayers = true;
+    public GUIStyle caca;
     [MenuItem("StickIt/TweakWindow")]
     static void Init()
     {
         // Get existing open window or if none, make a new one:
         TweakWindow window = (TweakWindow)EditorWindow.GetWindow(typeof(TweakWindow));
         window.Show();
+
     }
+
+    private void OnGUI()
+    {
+
+        showPlayers = EditorGUI.Foldout(new Rect(3, 3, position.width - 6, 15), showPlayers, "Player");
+        /*if (showPlayers)
+        {
+
+        }*/
+    }
+    
 }
