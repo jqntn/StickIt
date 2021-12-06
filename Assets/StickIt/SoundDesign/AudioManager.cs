2dcdea06aeb7c94544dcc136b8b6b71f7de8efd4
AlexCopin <alexcopin@live.fr>
2021-12-06 17:40:02 +0100

    feat: Add sounds & AudioManager

--- Assets/StickIt/SoundDesign/AudioManager.cs
+++ Assets/StickIt/SoundDesign/AudioManager.cs
@@ -2,9 +2,33 @@ using UnityEngine;
 
 public class AudioManager : MonoBehaviour
 {
-
-    public void PlayMusicMenu()
+    public static AudioManager instance;
+    private void Awake()
     {
-        AkSoundEngine.PostEvent("Event_M_Play_Menu", gameObject);
+        if (instance == null) instance = this;
+        else Destroy(gameObject);
+        DontDestroyOnLoad(this);
     }
+    public void PlayMusicMenu(GameObject go)
+    {
+        AkSoundEngine.PostEvent("Event_M_Play_Menu", go);
+    }
+    #region SFXSounds
+    public void PlayCollisionSounds(GameObject go)
+    {
+        AkSoundEngine.PostEvent("Play_SFX_S_Collision", go);
+    }
+    public void PlayJumpSounds(GameObject go)
+    {
+        AkSoundEngine.PostEvent("Play_SFX_S_Jump", go);
+    }
+    public void PlayLandSounds(GameObject go)
+    {
+        AkSoundEngine.PostEvent("Play_SFX_S_Land", go);
+    }
+    public void PlayDeathSounds(GameObject go)
+    {
+        AkSoundEngine.PostEvent("Play_SFX_S_Death", go);
+    }
+    #endregion
 }
