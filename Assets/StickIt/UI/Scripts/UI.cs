ffcf9f9bf4c912d1a57c233fc877cbe6651b52fd
jqntn <jqntn88@gmail.com>
2021-12-07 20:32:16 +0100

    feat(ui): update main menu

--- Assets/StickIt/UI/Scripts/UI.cs
+++ Assets/StickIt/UI/Scripts/UI.cs
@@ -1,13 +1,23 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 public class UI : MonoBehaviour
 {
+    [SerializeField]
+    private float secondsToPress;
     private void Start()
     { }
     private void Update()
     { }
-    public void Play() => SceneManager.LoadScene("1_MenuSelection");
-    public void Quit() => Application.Quit();
+    public void Play() => StartCoroutine(PressCoroutine(PlayFinal));
+    public void Quit() => StartCoroutine(PressCoroutine(QuitFinal));
+    public void QuitFinal() => Application.Quit();
+    public void PlayFinal() => SceneManager.LoadScene("1_MenuSelection");
+    public IEnumerator PressCoroutine(Action func)
+    {
+        yield return new WaitForSeconds(secondsToPress);
+        func?.Invoke();
+    }
 }
