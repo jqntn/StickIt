7574247bb3f1fb4e681f0eee81e95110f8219785
Julien QUENTIN <jqntn88@gmail.com>
2021-12-09 16:17:53 +0100

    fixes

--- Assets/StickIt/UI/Scripts/EnableOnSelect.cs
+++ Assets/StickIt/UI/Scripts/EnableOnSelect.cs
@@ -3,7 +3,7 @@ using UnityEngine.EventSystems;
 public class EnableOnSelect : MonoBehaviour, ISelectHandler, IDeselectHandler
 {
     [SerializeField] private GameObject toEnable;
-    private void Start() => toEnable.SetActive(false);
+    private void Awake() => toEnable.SetActive(false);
     public void OnDeselect(BaseEventData eventData) => toEnable.SetActive(false);
     public void OnSelect(BaseEventData eventData) => toEnable.SetActive(true);
 }
